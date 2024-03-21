67ec8553b8099b2b9a6aa4b7c0be2c828e56f419
nqathedev <130784933+nqathedev@users.noreply.github.com>
2024-03-21 21:06:48 +0700

    Create add-two-num.sh

---
+++ add-two-num.sh
@@ -0,0 +1,14 @@
+
+
+# Nhập hai số từ người dùng
+echo "Nhập số thứ nhất: "
+read num1
+
+echo "Nhập số thứ hai: "
+read num2
+
+# Tính tổng của hai số
+sum=$((num1 + num2))
+
+# Hiển thị kết quả
+echo "Tổng của $num1 và $num2 là: $sum"
